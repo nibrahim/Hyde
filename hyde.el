7b0e8e4896249bb29ccb79c03d564ee741aa3645
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-10 02:06:30 +0530

    Got font lock to work right

--- hyde.el
+++ hyde.el
@@ -18,6 +18,19 @@
 ;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 ;; 02111-1307, USA.
 
+(defconst hyde/hyde-version "0.1" 
+  "Hyde version")
+
+(defvar hyde-mode-hook nil
+  "Hook called by \"hyde-mode\"")
+
+
+(defvar hyde-mode-map
+  (let 
+      ((hyde-mode-map (make-keymap)))
+    ;; (define-key hyde-mode-map "\C-j" 'newline-and-indent)
+    hyde-mode-map)
+  "Keymap for Cisco router configuration major mode")
 
 ;; (defcustom hyde-home
 ;;   "/home/noufal/blog"
@@ -43,22 +56,54 @@
 
 (setq hyde/hyde-list-posts-command "/bin/ls -1tr")
 
+(defface hyde-header-face
+  '(
+    (((type tty) (class color)) (:foreground "blue" :weight 'bold))
+    (((type graphic) (class color)) (:foreground "lightsteelblue" :weight 'bold))
+    (t (:foreground "lightsteelblue" :weight "bold"))
+    )
+  "Face for a hyde header")
+
+(defvar hyde-header-face 'hyde-header-face "Face for a hyde header")
+
+(defconst hyde-font-lock-keywords
+  (list
+   '("^::.*" . hyde-header-face)
+   )
+  "Font lock keywords for Hyde mode")
+  
 
 (defun hyde/load-posts ()
   "Load up the posts and present them to the user"
   (interactive)
-  (switch-to-buffer (get-buffer-create "*Hyde*"))
-  (let (
-	(posts (split-string (shell-command-to-string
-			      (concat "cd " hyde-home "/" hyde-posts-dir " ; " hyde/hyde-list-posts-command )) "\n")))
-    (dolist (post posts)
-      (insert (concat post "\n"))
-      )))
+  ;; Insert headers
+  (insert ":: Editing blog at:" hyde-home "\n")
+  ;; Insert posts
+  (save-excursion
+    (let (
+	  (posts (split-string (shell-command-to-string
+				(concat "cd " hyde-home "/" hyde-posts-dir " ; " hyde/hyde-list-posts-command )) "\n")))
+      (dolist (post posts)
+	(insert (concat post "\n"))))
+    ;; Insert footer
+    (insert (concat ":: Hyde version " hyde/hyde-version "\n"))))
+
+(defun hyde/hyde-mode ()
+  "The Hyde major mode to edit Jekyll posts. I love how that sounds :)"
+  (interactive)
+  (kill-all-local-variables)
+  (use-local-map hyde-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(hyde-font-lock-keywords))
+  (setq major-mode 'hyde-mode
+	  mode-name "Hyde")
+  (run-hooks hyde-mode-hook))
 
 (defun hyde ()
   "Enters hyde mode"
   (interactive)
-  )
+  (switch-to-buffer (get-buffer-create "*Hyde*"))
+  (hyde/hyde-mode)
+  (hyde/load-posts))
   
 (provide 'hyde)
 
