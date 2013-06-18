c1a2821cee997d1771d3b5a9461862b2433c2161
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-06-18 11:28:42 +0530

    Don't run add if empty file list given.

--- hyde-git.el
+++ hyde-git.el
@@ -55,13 +55,25 @@
 
 (defun hyde/git/commit (repo files commit-message)
   "Commits the given files to the repository"
-  ; Add each of the files in the list
-  (dolist (f files)
-    (let ((cmd (format "cd '%s' && git add '%s'" (expand-file-name repo) f)))
-      (shell-command cmd)))
-  ; Commit them
-  (let ((cmd (format "cd '%s' && git commit -m '%s' > /dev/null" (expand-file-name repo) commit-message)))
-    (shell-command cmd)))
+  (if files
+      ;; If files gives, add each one and then commit it.
+      (progn
+        ;; Add each of the files in the list
+        (dolist (f files)
+          (message (concat "Dealing with " f))
+          (let ((cmd (format "cd '%s' && git add '%s'" (expand-file-name repo) f)))
+            (progn
+              (message (concat "Running " cmd))
+              (shell-command cmd))))
+        ;; Commit them
+        (let ((cmd (format "cd '%s' && git commit -m '%s' > /dev/null" (expand-file-name repo) commit-message)))
+          (progn
+            (message (concat "Running " cmd))
+            (shell-command cmd))))
+    ;; Otherwise, simply commit. Don't add. 
+    (let ((cmd (format "cd '%s' && git commit -m '%s' > /dev/null" (expand-file-name repo) commit-message)))
+      (shell-command cmd))))
+    
 
 (defun hyde/git/push (repo)
   "Pushes the repository"
